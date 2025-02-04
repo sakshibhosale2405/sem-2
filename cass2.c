eff8e9d76168baf8e27d5e60b3b37dea47ad4b6e
sakshibhosale2405 <sakshib2405@gmail.com>
2025-02-04 12:05:11 +0530

    Add files via upload

---
+++ cass2.c
@@ -0,0 +1,54 @@
+//#include<stdio.h>
+//int main()
+//{
+  //  char ch;
+    //printf("Enter any character:");
+    //ch=getchar();
+    //printf(" the Enter characteris%c",ch);
+
+    //return 0;
+
+//} 
+//printf(" the Enter string is%s:",str);
+//#include<stdio.h>
+//int main()
+//{
+   //char ch;
+   //printf("Enter any character:");
+  // ch=getchar();
+   //printf(" the Enter characteris:");
+   //putchar(ch);
+
+    //return 0;
+
+//} 
+
+//#include<stdio.h>
+//int main()
+//{
+   //char str[100];
+   //printf("Enter any string:");
+  // scanf("%s",&str);
+   //printf(" the Enter string is%s:",str);
+   
+
+    //return 0;
+
+//} 
+
+//#include<stdio.h>
+//int main()
+//{
+   
+  //printf("%6d\n",123);
+  //printf("%6d\n",12345);
+  //printf("%6.2f\n",12.34);
+   
+   
+
+  //return 0;
+
+//} 
+
+
+
