234837e5ac0dda06b36f641f7ff395cb2eac82b5
sakshibhosale2405 <sakshib2405@gmail.com>
2025-02-26 16:47:15 +0530

    greater no in 2 no.s

---
+++ greater no in 2 no.s
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int num1,num2;
+    printf("enter num1 and num2:");
+    scanf("%d %d",&num1,&num2);
+    
+   
+     if (num1>num2)
+   
+{
+      
+       printf("%d is grater",num1);
+    
+         
+    }
+   if (num2>num1)
+ {
+     printf("%d is greater ",num2);
+ }
+ if (num1==num2)
+ {
+     printf("both num1 and num2 is equal");
+ }
+ 
+ 
+ 
+ 
+    return 0;
+}
